6cea33e0d49f2268ed15b247ce86ca5831830855
Phil Tanner <phil.github@philtanner.com>
2017-10-09 16:59:47 +1300

    Sanitising user input.

--- admin/admin.php
+++ admin/admin.php
@@ -68,51 +68,53 @@
 					$show_email_received = false;
 					// First off, grab what our settings are now
 					$settings = array();
-					$settings["wppt_emailer_smtpdebug"]  = get_option("wppt_emailer_smtpdebug" );
-					$settings["wppt_emailer_smtp_host"]  = get_option("wppt_emailer_smtp_host" );
-					$settings["wppt_emailer_smtp_auth"]  = get_option("wppt_emailer_smtp_auth" );
-					$settings["wppt_emailer_port"]       = get_option("wppt_emailer_port"      );
-					$settings["wppt_emailer_username"]   = get_option("wppt_emailer_username"  );
-					$settings["wppt_emailer_password"]   = get_option("wppt_emailer_password"  );
+					$settings["wppt_emailer_smtpdebug"]  = (int)get_option("wppt_emailer_smtpdebug");
+					$settings["wppt_emailer_smtp_host"]  = get_option("wppt_emailer_smtp_host");
+					$settings["wppt_emailer_smtp_auth"]  = get_option("wppt_emailer_smtp_auth");
+					$settings["wppt_emailer_port"]       = (int)get_option("wppt_emailer_port");
+					$settings["wppt_emailer_username"]   = get_option("wppt_emailer_username");
+					$settings["wppt_emailer_password"]   = get_option("wppt_emailer_password");
 					$settings["wppt_emailer_smtpsecure"] = get_option("wppt_emailer_smtpsecure");
 					
 					// Then update them to what we've asked for to carry out our test
-					update_option("wppt_emailer_smtpdebug",  4 ); // Except we always want most output while testing
-					update_option("wppt_emailer_smtp_host",  $_POST["wppt_emailer_smtp_host"] );
-					update_option("wppt_emailer_smtp_auth",  $_POST["wppt_emailer_smtp_auth"] );
-					update_option("wppt_emailer_port",       $_POST["wppt_emailer_port"]      );
-					update_option("wppt_emailer_username",   $_POST["wppt_emailer_username"]  );
-					update_option("wppt_emailer_password",   $_POST["wppt_emailer_password"]  );
-					update_option("wppt_emailer_smtpsecure", $_POST["wppt_emailer_smtpsecure"]);
+					update_option("wppt_emailer_smtpdebug",  4 ); // We're always going to test with max debug on!
+					update_option("wppt_emailer_smtp_host",  sanitize_option("wppt_emailer_smtp_host", $_POST["wppt_emailer_smtp_host"]) );
+					update_option("wppt_emailer_smtp_auth",  sanitize_option("wppt_emailer_smtp_auth", $_POST["wppt_emailer_smtp_auth"]) );
+					update_option("wppt_emailer_port",       (int)$_POST["wppt_emailer_port"] );
+					update_option("wppt_emailer_username",   sanitize_option("wppt_emailer_username", $_POST["wppt_emailer_username"]) );
+					update_option("wppt_emailer_password",   sanitize_option("wppt_emailer_password", $_POST["wppt_emailer_password"]) );
+					update_option("wppt_emailer_smtpsecure", sanitize_option("wppt_emailer_smtpsecure", $_POST["wppt_emailer_smtpsecure"]) );
 					
 					// Then start capturing our output just before we try sending the email
+					$mailoutput = '<strong>Server output:</strong>'."\n";
 					ob_start();
 					$mail = wp_mail( WPPT_EMAILER_TEST_TO_ADDR, WPPT_EMAILER_TEST_SUBJECT, WPPT_EMAILER_TEST_MESSAGE );
-					$mailoutput = ob_get_contents();
+					$mailoutput .= ob_get_contents();
 					ob_end_clean();
 					
 					// Then reset our options to what they were (we were testing, not saving after all) and we don't want live cux
 					// getting wrong settings.
-					update_option("wppt_emailer_smtpdebug",  $settings["wppt_emailer_smtpdebug"] );
+					// No sanitising, because we're only putting back what we had when we pulled it out. Save some CPU
+					update_option("wppt_emailer_smtpdebug",  (int)$settings["wppt_emailer_smtpdebug"] );
 					update_option("wppt_emailer_smtp_host",  $settings["wppt_emailer_smtp_host"] );
 					update_option("wppt_emailer_smtp_auth",  $settings["wppt_emailer_smtp_auth"] );
-					update_option("wppt_emailer_port",       $settings["wppt_emailer_port"]      );
+					update_option("wppt_emailer_port",       (int)$settings["wppt_emailer_port"]      );
 					update_option("wppt_emailer_username",   $settings["wppt_emailer_username"]  );
 					update_option("wppt_emailer_password",   $settings["wppt_emailer_password"]  );
 					update_option("wppt_emailer_smtpsecure", $settings["wppt_emailer_smtpsecure"]);
 					
 					// Now, see if we can see any issues we can help you start debugging
 					if( strpos($mailoutput, 'No such host is known') !== false ) {
-						throw new wppt_emailer_Exception_Local(sprintf(__('Unable to resolve the SMTP Host "%s". Check your internet connection or that you have entered the hostname correctly.','wppt_emailer'), $_POST["wppt_emailer_smtp_host"]));
+						throw new wppt_emailer_Exception_Local(sprintf(__('Unable to resolve the SMTP Host "%s". Check your internet connection or that you have entered the hostname correctly.','wppt_emailer'), sanitize_text_field($_POST["wppt_emailer_smtp_host"])));
 					}
 					if( strpos($mailoutput, '10061') !== false || strpos($mailoutput, 'No connection could be made because the target machine actively refused it.') !== false ) {
-						throw new wppt_emailer_Exception_Remote_Refused(sprintf(__('The remote server actively refused our connection. SMTP Host "%s" is not listening on port %d. Check your settings and try again.','wppt_emailer'), $_POST["wppt_emailer_smtp_host"], $_POST["wppt_emailer_port"]));
+						throw new wppt_emailer_Exception_Remote_Refused(sprintf(__('The remote server actively refused our connection. SMTP Host "%s" is not listening on port %d. Check your settings and try again.','wppt_emailer'), sanitize_text_field($_POST["wppt_emailer_smtp_host"]), (int)$_POST["wppt_emailer_port"]));
 					}
 					if( strpos($mailoutput, '530-5.5.1 Authentication Required.') !== false ) {
-						throw new wppt_emailer_Exception_Remote_Require_Authentication(sprintf(__('Couldn\'t connect to SMTP Host "%s" on port %d. Username and password is required.','wppt_emailer'), $_POST["wppt_emailer_smtp_host"], $_POST["wppt_emailer_port"]));
+						throw new wppt_emailer_Exception_Remote_Require_Authentication(sprintf(__('Couldn\'t connect to SMTP Host "%s" on port %d. Username and password is required.','wppt_emailer'), sanitize_text_field($_POST["wppt_emailer_smtp_host"]), (int)$_POST["wppt_emailer_port"]));
 					}
 					if( strpos($mailoutput, '535-5.7.8 Username and Password not accepted.') !== false ) {
-						throw new wppt_emailer_Exception_Remote_Incorrect_Credentials(sprintf(__('Couldn\'t connect to SMTP Host "%s" on port %d. Username and password is incorrect.','wppt_emailer'), $_POST["wppt_emailer_smtp_host"], $_POST["wppt_emailer_port"]));
+						throw new wppt_emailer_Exception_Remote_Incorrect_Credentials(sprintf(__('Couldn\'t connect to SMTP Host "%s" on port %d. Username and password is incorrect.','wppt_emailer'), sanitize_text_field($_POST["wppt_emailer_smtp_host"]), (int)$_POST["wppt_emailer_port"]));
 					}
 					if( strpos($mailoutput, '534 5.7.14  https://support.google.com/mail/answer/78754') !== false ) {
 						throw new wppt_emailer_Exception_Remote_Unknown_Auth(__('Unknown authentication method. Try TLS? Or enable less secure apps: https://support.google.com/accounts/answer/6010255.','wppt_emailer'));
@@ -128,13 +130,13 @@
 					
 					$show_email_received = true;
 				} elseif( $_POST['action'] == 'save' ) {
-					update_option("wppt_emailer_smtpdebug",  $_POST["wppt_emailer_smtpdebug"] );
-					update_option("wppt_emailer_smtp_host",  $_POST["wppt_emailer_smtp_host"] );
-					update_option("wppt_emailer_smtp_auth",  $_POST["wppt_emailer_smtp_auth"] );
-					update_option("wppt_emailer_port",       $_POST["wppt_emailer_port"]      );
-					update_option("wppt_emailer_username",   $_POST["wppt_emailer_username"]  );
-					update_option("wppt_emailer_password",   $_POST["wppt_emailer_password"]  );
-					update_option("wppt_emailer_smtpsecure", $_POST["wppt_emailer_smtpsecure"]);
+					update_option("wppt_emailer_smtpdebug",  (int)$_POST["wppt_emailer_smtpdebug"]);
+					update_option("wppt_emailer_smtp_host",  sanitize_option("wppt_emailer_smtp_host", $_POST["wppt_emailer_smtp_host"]) );
+					update_option("wppt_emailer_smtp_auth",  sanitize_option("wppt_emailer_smtp_auth", $_POST["wppt_emailer_smtp_auth"]) );
+					update_option("wppt_emailer_port",       (int)$_POST["wppt_emailer_port"]                  );
+					update_option("wppt_emailer_username",   sanitize_option("wppt_emailer_username", $_POST["wppt_emailer_username"])  );
+					update_option("wppt_emailer_password",   sanitize_option("wppt_emailer_password", $_POST["wppt_emailer_password"])  );
+					update_option("wppt_emailer_smtpsecure", sanitize_option("wppt_emailer_smtpsecure", $_POST["wppt_emailer_smtpsecure"] ) );
 				}
 				
 			} catch( wppt_emailer_Exception_Remote_Refused $Ex ) {
@@ -398,7 +400,7 @@
 					title: "Logfile: "+log,
 					modal: true,
 					width: "80%",
-					close: function(){ $(this).dialog("destroy"); }
+					close: function(){ jQuery(this).dialog("destroy"); }
 				}).parent().css({ zIndex:10000 });
 			}
 		</script>
